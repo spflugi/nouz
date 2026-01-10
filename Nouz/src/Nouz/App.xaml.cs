e767acf06ff47c66db9500d08b143ae089e7f127
spflugi <info@pflugi.li>
2026-01-10 17:32:50 +0100

    Added a global exception handler

--- Nouz/src/Nouz/App.xaml.cs
+++ Nouz/src/Nouz/App.xaml.cs
@@ -6,11 +6,14 @@ namespace Nouz
     {
         private readonly ILoggerAdapter<App> _logger;
 
+        private Exception? _lastFirstChanceException;
+
         public App(ILoggerAdapter<App> logger)
         {
             _logger = logger;
 
             InitializeComponent();
+            CatchUnhandledExceptions();
         }
 
         protected override void OnStart()
@@ -31,6 +34,65 @@ namespace Nouz
         protected override Window CreateWindow(IActivationState? activationState)
         {
             return new Window(new MainPage()) { Title = "Nouz" };
+        }
+
+        private void CatchUnhandledExceptions()
+        {
+            TaskScheduler.UnobservedTaskException += (sender, args) => LogFatalException(args.Exception, $"Unhandled TaskScheduler exception from {sender?.ToString() ?? "Unknown"}");
+
+            AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+            {
+                if (args.ExceptionObject is not Exception exception)
+                {
+                    // ignore if exception object is not an exception
+                    return;
+                }
+
+                LogFatalException(exception, $"Unhandled app domain exception from {sender?.ToString() ?? "Unknown"}");
+            };
+
+            // For WinUI 3:
+            //
+            // * Exceptions on background threads are caught by AppDomain.CurrentDomain.UnhandledException,
+            //   not by Microsoft.UI.Xaml.Application.Current.UnhandledException
+            //   See: https://github.com/microsoft/microsoft-ui-xaml/issues/5221
+            //
+            // * Exceptions caught by Microsoft.UI.Xaml.Application.Current.UnhandledException have details removed,
+            //   but that can be worked around by saved by trapping first chance exceptions
+            //   See: https://github.com/microsoft/microsoft-ui-xaml/issues/7160
+            //
+            AppDomain.CurrentDomain.FirstChanceException += (sender, args) =>
+            {
+                // First-chance exceptions are caught here very early, even before reaching a potentially
+                // existing catch block. If they are not caught, they should also show up in the handler 
+                // below - but that one apparently does not have the stack trace then. See here for more 
+                // details: https://gist.github.com/mattjohnsonpint/7b385b7a2da7059c4a16562bc5ddb3b7
+                _lastFirstChanceException = args.Exception;
+            };
+
+            Microsoft.UI.Xaml.Application.Current.UnhandledException += (sender, args) =>
+            {
+                var exception = args.Exception;
+
+                if (exception.StackTrace is null && _lastFirstChanceException is not null)
+                {
+                    exception = _lastFirstChanceException!;
+                }
+
+                LogFatalException(exception, $"Unhandled UI Xaml exception from {sender?.ToString() ?? "Unknown"}");
+            };
+        }
+
+        private void LogFatalException(Exception exception, string message)
+        {
+            try
+            {
+                _logger.LogError(exception, message);
+            }
+            catch
+            {
+                // just suppress any error logging exceptions
+            }
         }
     }
 }
