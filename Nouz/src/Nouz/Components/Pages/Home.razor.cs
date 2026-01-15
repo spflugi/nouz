a618329b7b9da246821cd639b21ec65632ad95a2
spflugi <info@pflugi.li>
2026-01-15 19:52:01 +0100

    Introduce shared base component and observable extensions

---
+++ Nouz/src/Nouz/Components/Pages/Home.razor.cs
@@ -0,0 +1,6 @@
+﻿namespace Nouz.Components.Pages;
+
+public partial class Home
+{
+    
+}
---
+++ Nouz/src/Nouz/Components/SharedComponentBase.cs
@@ -0,0 +1,21 @@
+﻿using Mediator;
+using Microsoft.AspNetCore.Components;
+using Nouz.Application.Store;
+
+namespace Nouz.Components;
+
+public abstract class SharedComponentBase : ComponentBase, IDisposable
+{
+    public event EventHandler? DisposingEvent;
+
+    [Inject]
+    protected IMediator Mediator { get; init; } = null!;
+
+    [Inject]
+    protected IStateProvider StateProvider { get; init; } = null!;
+
+    public virtual void Dispose()
+    {
+        DisposingEvent?.Invoke(this, EventArgs.Empty);
+    }
+}
