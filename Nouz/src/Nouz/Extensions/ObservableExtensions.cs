8e9713f3b92c580cc7b81df3e142fbdaabe35e18
spflugi <info@pflugi.li>
2026-01-15 19:52:01 +0100

    Introduce shared base component and observable extensions

---
+++ Nouz/src/Nouz/Extensions/ObservableExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Reactive.Linq;
+using Nouz.Components;
+
+namespace Nouz.Extensions;
+
+public static class ObservableExtensions
+{
+    public static IObservable<T> TakeUntilDisappearing<T>(this IObservable<T> observable, SharedComponentBase component)
+    {
+        var disposing = Observable.FromEventPattern<EventHandler, EventArgs>(
+            handler => component.DisposingEvent += handler,
+            handler => component.DisposingEvent -= handler);
+
+        return observable.TakeUntil(disposing);
+    }
+}
